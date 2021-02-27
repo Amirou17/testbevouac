57663e99ea8ca24c28e54a361b95c80ee82ed01a
Amirou17 <damirou95@gmail.com>
2021-02-27 08:11:08 +0100

    module1 sans minicarte

--- testapi/src/App.vue
+++ testapi/src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <Module1 msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Module1 from './components/Module1.vue'
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Module1
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ testapi/src/components/Module1.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container pt-5">
+    <h2 class="bg-info text-muted sticky-top">Les 10 premières entreprises à fort potentiel d'embauche se trouvant à Paris ou Marseille</h2>
+    <div class="row row-cols-1 row-cols-md-2 pt-5">
+      <div class="container" :key="index" v-for="(entreprise, index) in entreprise">
+        <div class="jumbotron col mb-7">
+          <h3>{{ entreprise.name }}</h3>
+          <hr>
+          <h5>Nombre d'étoiles : <span class="badge badge-info">{{ entreprise.stars }}</span></h5>
+          <h6>Adresse : {{ entreprise.address }}</h6>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'module1',
+  data(){
+    return{
+      config : {
+        headers: {
+          'Authorization' : 'Bearer nRHarXsG5TCW_Y7n95P7dnqtnwY'
+        }
+      },
+      entreprise: null
+    }
+  },
+  mounted() {
+    axios
+        .get('https://api.emploi-store.fr/partenaire/labonneboite/v1/company/?rome_codes_keyword_search=Informatique&page=1&page_size=10&departments=75,13', this.config)
+        .then((reponse) => {
+          this.entreprise = reponse.data.companies;
+          //console.log(this.entreprise)
+        }).catch((reason) => {console.log(reason)});
+  }
+}
+</script>
+
+<style scoped>
+</style>
