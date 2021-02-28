82b7907226f8dd45939d06ac782f6544f0ab91e2
Amirou17 <damirou95@gmail.com>
2021-02-28 11:01:07 +0100

    Dernier commit module2

--- testapi/src/components/Module2.vue
+++ testapi/src/components/Module2.vue
@@ -2,11 +2,14 @@
   <div class="container w-75 pt-3">
     <div class="list row">
       <div class="col">
-        <h4 class="pb-3">Listes des entreprises à fort potentiel d'embauche se trouvant à Paris ou Marseille</h4>
+        <h4 class="pb-3">
+          Listes des entreprises à fort potentiel d'embauche se trouvant à Paris ou Marseille travaillant dans le secteur
+          Informatique
+        </h4>
         <ul class="list-group" id="tutorials-list">
           <li
               class="list-group-item"
-              :class="{ active: index === currentIndex }"
+              :class="{ active: index === indexCourant }"
               v-for="(entreprise, index) in entreprises"
               :key="index"
           >
@@ -17,8 +20,8 @@
       <div class="col-md-12 pt-2 ">
         <b-pagination
             v-model="page"
-            :total-rows="count"
-            :per-page="pageSize"
+            :total-rows="nbEntreprises"
+            :per-page="nbEntreprisesParPage"
             prev-text="Prev"
             next-text="Next"
             @change="handlePageChange"
@@ -36,15 +39,15 @@ export default {
     return {
       config : {
         headers: {
-          'Authorization' : 'Bearer Cvu0vjpc42qOCnvmMHVoBHoKqoY'
+          'Authorization' : 'Bearer sh6E1PhnqmslbohYOwgXt3c3XMA'
         }
       },
       entreprise: null,
       entreprises: [],
-      currentIndex: -1,
+      indexCourant: -1,
       page: 1,
-      count: 0,
-      pageSize: 10,
+      nbEntreprises: 0,
+      nbEntreprisesParPage: 10,
     }
   },
   methods: {
@@ -57,9 +60,9 @@ export default {
           .get('https://api.emploi-store.fr/partenaire/labonneboite/v1/company/?rome_codes_keyword_search=Informatique&page='+this.page+'&page_size=10&departments=75,13', this.config)
           .then((reponse) => {
             this.entreprises = reponse.data.companies;
-            this.count = reponse.data.companies_count;
+            this.nbEntreprises = reponse.data.companies_count;
           }).catch((reason) => {
-        this.count = 200;
+            
         console.log(reason)
       });
     }
